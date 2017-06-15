52643fdd5972101fd30cf12c165044c952bd9e29
sravanikalluri <sravanikalluri5@gmail.com>
2017-06-15 10:10:03 -0500

    initial commit

---
+++ src/example.js
@@ -0,0 +1,22 @@
+function validate() {
+    var Firstname = document.getElementById("txt-name");
+    if (Firstname.value === "") {
+        alert("please enter the firstname");
+    }
+    var lastname = document.getElementById("txt-name");
+    if (lastname.value === "") {
+        alert("please enter the lastname");
+    }
+    var Bachelordegree = document.getElementById("txt-name");
+    if (Bachelordegree.value === "") {
+        alert("please enter the bachelor degree");
+    }
+    var Emailid = document.getElementById("txt-name");
+    if (Emailid.value === "") {
+        alert("please enter the emailid");
+    }
+    var phoneno = document.getElementById("txt-name");
+    if (phoneno.value === "") {
+        alert("please enter the phone no");
+    }
+}
---
+++ src/example.ts
@@ -0,0 +1,25 @@
+
+function validate(){
+    var Firstname:any=document.getElementById("txt-name");
+    if(Firstname.any ===""){
+        alert("please enter the firstname")
+    }
+    var lastname:any=document.getElementById("txt-name");
+    if(lastname.any ===""){
+        alert("please enter the lastname" )
+    }
+    var Bachelordegree:any=document.getElementById("txt-name");
+    if (Bachelordegree.any ==="" ){
+        alert("please enter the bachelor degree")
+    }
+    var Emailid:any = document.getElementById("txt-name");
+    if(Emailid.any ==="" ){
+        alert("please enter the emailid")
+    }
+    var phoneno:any = document.getElementById("txt-name");
+    if(phoneno.any ==="" ){
+        alert("please enter the phone no")
+
+    }
+
+}
