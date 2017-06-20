f1f6980cb1d6e05f65d40bbe61d73b45be52bfff
sravanikalluri <sravanikalluri5@gmail.com>
2017-06-20 15:25:18 -0500

    Initial commit

---
+++ src/function.js
@@ -0,0 +1,10 @@
+function student(fname,lname,age,school){
+this.fname=fname;
+this.lname=lname;
+this.age=age;
+this.school=school;
+}
+
+var f1 = new student("sravani","kalluri",23,"tamuk");
+console.log(f1);
+
